af26bb27ba2addef2ad784459b10863cede4d9bf
Elijah Miernicki <miernickielijah@gmail.com>
2021-05-18 09:44:45 -0500

    changing directory

---
+++ javascript.js
@@ -0,0 +1,121 @@
+var userFormEl = document.querySelector('#user-form');
+var formatBtn = document.querySelector('.dropdown');
+var nameInputEl = document.querySelector('#username');
+var repoContainerEl = document.querySelector('#repos-container');
+var repoSearchTerm = document.querySelector('#repo-search-term');
+
+
+// console.log($.onclick(formatBtn).value)
+
+$('.dropdown-menu a').on('onclick', function () {
+    var txt = ($(this).text());
+    alert("Your Favourite Sports is " + txt);
+});
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+
+        repoContainerEl.textContent = '';
+        nameInputEl.value = '';
+    } else {
+        alert('Please enter a GitHub username');
+    }
+};
+
+var buttonClickHandler = function (event) {
+    // What is `event.target` referencing?
+    // TODO: Write your answer here
+    var format = event.target.getAttribute('data-format');
+
+    // Why is this `if` block in place?
+    // TODO: Write your answer here
+    if (format) {
+        getFeaturedRepos(format);
+
+        repoContainerEl.textContent = '';
+    }
+};
+
+var getUserRepos = function (user) {
+    var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                    displayRepos(data, user);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to library of congress');
+        });
+};
+
+var getFormat = function (format) {
+
+    var apiUrl = '' + format + '';
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data.items, format);
+            });
+        } else {
+            alert('Error: ' + response.statusText);
+        }
+    });
+};
+
+var displayRepos = function (repos, searchTerm) {
+    if (repos.length === 0) {
+        repoContainerEl.textContent = 'No repositories found.';
+        // What would happen if there was no `return;` here?
+        // TODO: Write your answer here
+        return;
+    }
+
+    repoSearchTerm.textContent = searchTerm;
+
+    for (var i = 0; i < repos.length; i++) {
+        // What is the result of this string concatenation?
+        // TODO: Write your answer here
+        var repoName = repos[i].owner.login + '/' + repos[i].name;
+
+        var repoEl = document.createElement('div');
+        repoEl.classList = 'list-item flex-row justify-space-between align-center';
+
+        var titleEl = document.createElement('span');
+        titleEl.textContent = repoName;
+
+        repoEl.appendChild(titleEl);
+
+        var statusEl = document.createElement('span');
+        statusEl.classList = 'flex-row align-center';
+
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML =
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        repoEl.appendChild(statusEl);
+
+        repoContainerEl.appendChild(repoEl);
+    }
+};
+
+userFormEl.addEventListener('submit', formSubmitHandler);
+
+
+formatBtn.addEventListener('click', buttonClickHandler);
